41fb7e0efaf523da4da0c6ed31d5f9a00441dba5
Jose <joseangarita1126@gmail.com>
2024-09-18 17:28:06 -0500

    Primer commit

--- Main.java
+++ Main.java
@@ -1,9 +1,4 @@
 
-/*
-Online Java - IDE, Code Editor, Compiler
-
-Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
-*/
 import java.util.Scanner;
 public class Main
 {
@@ -19,46 +14,63 @@ public class Main
         int numStudents;
         System.out.println("Digite la cantidad de alumnos ");
         numStudents = scanner.nextInt();
+        System.printLn(numStudents);
         //Salto de Linea
         scanner.nextLine();
         // Declara las variables que contendrán los arrays
         String[] names = new String[numStudents];
-        String[] identification = new String[numStudents];
+        int[] identification = new int[numStudents];
         double[][] notes = new double[numStudents][SUBJECT];
+        String subject[] = {"Cálculo", "Programación", "Algebra"};
+        // Asignamos manualmente los nombres de las tres asignaturas
 
         // Recorre tantas veces los alumnos que se les digito
-        for(int i=0; i<numStudents; i++){
-            //Pide la identificacion
-            System.out.println("Nombre del estudiante " + (i + 1));
-            names[i] = scanner.nextLine();
-             //Pide la identificacion
-            System.out.println("Identificación del estudiante " + (i + 1));
-            identification[i] = scanner.nextLine();
-            //Pide las notas de las asignaturas
-            System.out.println("Ingrese la nota de cada Asignatura del estudiante " + (i + 1) + " (Son " + SUBJECT + " Asignaturas)");
-            System.out.println("De 0.0 a 5.0");
-            // Recorre tantas veces la cantidad de asignaturas
-            for(int j=0; j<SUBJECT; j++){
-                notes[i][j] = scanner.nextDouble();
-            }
-            //Salto de Linea
+        int i = 0;
+        for(i = 0; i < names.length; i++) 
+        { //for para ingresar los datos y llenar cada array
+
+            System.out.println();
+
+            System.out.print("Ingrese el nombre del estudiante " + (i+1) + ": ");
+            names[i] = scanner.nextLine();  //llenar array de nombres
+
+            
+            System.out.print("Ingrese la identificación del estudiante: ");
+            identification[i] = scanner.nextInt(); //llenar array de codigos
+            
+            // Consumir la nueva línea sobrante después de nextInt
             scanner.nextLine();
+
+            double sumNotes = 0;
+            for (int j = 0; j < SUBJECT; j++) { //for anidado para llenar la matriz de notas
+                do { //uso de do while para verificar que la nota ingresada este entre 0.0 y 5.0
+                    System.out.print("Ingrese la nota " + subject[j] + " de " + names[i] + " entre 0.0 y 5.0: ");
+                    notes[i][j] = scanner.nextDouble();
+                    if (notes[i][j]<0.0 || notes[i][j]>5.0 ) {
+                        System.out.println("Nota invalida, debe ingresar una nota entre 0.0 y 5.0");
+                    }
+                    
+                } while (notes[i][j]<0.0 || notes[i][j]>5.0);
+
+                sumNotes += notes[i][j]; //contador que almacena la suma de las notas
+            }
+            scanner.nextLine();
+
         }
+        // Consumir la nueva línea sobrante después de la última entrada de nota
         
         //Calcular y hacer reporte
         System.out.println("REPORTE FINAL:");
-        for (int i = 0; i < numStudents; i++) {
+        
+        for (i = 0; i < numStudents; i++) {
             double average = calculateAverage(notes[i]);
             //Hace una especie de if para saber si aprobó o no
             String state = average >= APPROVED ? "Aprobado" : "Reprobado";
             System.out.println(names[i] + "(ID: " + identification[i] + ") - " + "Promedio: " + average + " - " + state);
         }
-        
-        
     }
-    
     // Método para calcular el promedio de las notas
-    private static double calculateAverage(double[] notes) {
+    public static double calculateAverage(double[] notes) {
         double sum = 0;
         for (double note : notes) {
             sum += note;
